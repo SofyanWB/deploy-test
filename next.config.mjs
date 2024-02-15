be12c7528ac195e071447d8820b7eb9c52f39b4b
SofyanWB <sofyanwibowo@outlook.com>
2024-02-15 14:30:50 +0700

    3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/deploy-test",
+    output: 'export',
+    reactStrictMode: true,
+    basePath: '/deploy-test',
 
     images: {
         unoptimized: true,
